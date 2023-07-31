913db6ecd1fef786f5f76e386734f519de9581d0
Bikathi Martin <bikathimartin@outlook.com>
2023-07-31 19:39:41 +0300

    Test to confirm Tailwind is well installed

--- src/App.vue
+++ src/App.vue
@@ -3,4 +3,7 @@
 <template>
 	<h1 class="text-red-500 underline text-5xl">Hello world</h1>
 	<h2>I am here</h2>
+	<h3 class="text-blue-900 underline text-[100px] italic">My name is Martin</h3>
 </template>
+
+
