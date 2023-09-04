ee1df3c7c3da3692309142de65d5ae3fab09277c
Bikathi Martin <bikathimartin@outlook.com>
2023-09-04 21:59:44 +0300

    Remove links to and router configs

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import router from './configs/router.js'
 
-createApp(App).use(router).mount('#app')
+createApp(App).mount('#app')
