e5f679db8bbec8f242de656dd11c846e5e7da5f7
Bikathi Martin <bikathimartin@outlook.com>
2023-10-09 15:16:51 +0300

    Create context menu for managing meme text

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <script setup>
-	import { ref } from "vue";
+	import { reactive, ref } from "vue";
 	import UploadIcon from "./components/UploadIcon.vue";
 
 	const imagePreviewlink = ref("");
 	const memeText = ref(
 		"This is the text that appears on your meme. Click on it to edit to your own text. Color, size and fonts can be edited later in step 3"
 	);
+	const contextMenuX = ref(0);
+	const contextMenuY = ref(0);
+	const showContextMenu = ref(false);
+	const memeTextStyles = reactive({
+		fontColor: "",
+		fontSize: "",
+		fontFace: "",
+	});
 
 	function handleImageUpload(event) {
 		const file = event.target.files[0];
@@ -20,6 +28,12 @@
 			imagePreviewlink.value = event.target.result;
 		};
 		reader.readAsDataURL(file);
+	}
+
+	function openContextMenu(event) {
+		contextMenuX.value = event.clientX;
+		contextMenuY.value = event.clientY;
+		showContextMenu.value = true;
 	}
 </script>
 
@@ -83,12 +97,102 @@
 						>Step 2: Add Your Meme Text</span
 					>
 				</div>
-				<div class="collapse-content">
+				<div class="collapse-content relative">
 					<p
-						class="outline-none text-xl font-roboto text-center text-secondary"
-						contenteditable="true">
+						class="outline-none font-roboto text-center text-secondary"
+						:class="{
+							[memeTextStyles.fontColor]: true,
+							[memeTextStyles.fontSize]: true,
+							[memeTextStyles.fontFace]: true,
+						}"
+						contenteditable="true"
+						@contextmenu.prevent="openContextMenu($event)">
 						{{ memeText }}
 					</p>
+					<div
+						v-if="showContextMenu"
+						class="border rounded-lg bg-base-300 w-fit p-2 [&>h1]:text-lg [&>h1]:font-bold [&>h2]:uppercase [&>h2]:font-roboto [&>h2]:text-sm [&>h2]:py-1 [&>h1]:py-1">
+						<h1>Text Options</h1>
+						<h2>Color</h2>
+						<ul
+							class="flex items-center space-x-2 bg-base-200 p-2 rounded-md">
+							<li
+								v-for="(option, index) in [
+									{
+										background: 'bg-red-500',
+										text: 'text-red-500',
+									},
+									{
+										background: 'bg-green-500',
+										text: 'text-green-500',
+									},
+									{
+										background: 'bg-yellow-500',
+										text: 'text-yellow-500',
+									},
+									{
+										background: 'bg-pink-500',
+										text: 'text-pink-500',
+									},
+									{
+										background: 'bg-teal-500',
+										text: 'text-teal-500',
+									},
+									{
+										background: 'bg-white',
+										text: 'text-white',
+									},
+								]"
+								:key="index"
+								:class="option.background"
+								class="w-6 h-6 rounded-md cursor-pointer"
+								@click.prevent="
+									() => {
+										memeTextStyles.fontColor = option.text;
+									}
+								"></li>
+						</ul>
+						<h2>Size</h2>
+						<ul class="bg-base-200 p-2 rounded-md">
+							<li
+								v-for="(option, index) in [
+									{ text: 'small', style: 'text-sm' },
+									{ text: 'normal', style: 'text-base' },
+									{ text: 'large', style: 'text-lg' },
+									{ text: 'x-large', style: 'text-xl' },
+									{ text: 'xx-large', style: 'text-2xl' },
+								]"
+								:key="index"
+								class="cursor-pointer hover:underline"
+								:class="option.style"
+								@click.prevent="
+									() => {
+										memeTextStyles.fontSize = option.style;
+									}
+								">
+								{{ option.text }}
+							</li>
+						</ul>
+						<h2>Font Style</h2>
+						<ul class="bg-base-200 p-2 rounded-md">
+							<li
+								v-for="(option, index) in [
+									{ text: 'Roboto', style: 'font-roboto' },
+									{ text: 'Poppins', style: 'font-poppins' },
+								]"
+								:key="index"
+								:class="option.style"
+								class="cursor-pointer hover:underline"
+								@click.prevent="
+									() => {
+										memeTextStyles.fontFace = option.style;
+									}
+								">
+								{{ option.text }}
+							</li>
+						</ul>
+						<h1>Image Options</h1>
+					</div>
 				</div>
 			</div>
 			<div class="collapse bg-base-200">
