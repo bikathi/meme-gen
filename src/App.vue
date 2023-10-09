296dc3211fc202078407e0bf4ba6c0e0cd043e23
Bikathi Martin <bikathimartin@outlook.com>
2023-10-09 13:02:57 +0300

    Add rounding to preview image

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 	import { ref } from "vue";
+	import UploadIcon from "./components/UploadIcon.vue";
 
 	const imagePreviewlink = ref("");
 	function handleImageUpload(event) {
@@ -48,19 +49,7 @@
 							class="mt-2 flex bg-base-300 hover:bg-base-300/60 flex-col items-center justify-center border-2 border-gray-300 border-dashed cursor-pointer h-72 w-96 rounded-md">
 							<div
 								class="flex flex-col items-center justify-center pt-5 pb-6">
-								<svg
-									aria-hidden="true"
-									class="w-10 h-10 mb-3 text-gray-400"
-									fill="none"
-									stroke="currentColor"
-									viewBox="0 0 24 24"
-									xmlns="http://www.w3.org/2000/svg">
-									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										stroke-width="2"
-										d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path>
-								</svg>
+								<UploadIcon />
 								<p
 									class="mb-2 text-sm text-gray-500 dark:text-gray-400">
 									<span class="font-semibold"
@@ -78,7 +67,7 @@
 					<img
 						:src="imagePreviewlink"
 						alt="image-preview"
-						class="font-poppins" />
+						class="rounded-lg w-56 h-56" />
 				</div>
 			</div>
 			<div class="collapse bg-base-200">
